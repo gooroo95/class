245c8c6fb329e54cfd9b4902e838c421560a5729
Brad Nichols <gooroo95@gmail.com>
2018-02-07 13:27:36 +0000

    Added Devise Database

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,20 @@
 class HomeController < ApplicationController
   def index
+    if params[:id] == "" 
+      @nothing = "Hey,you forgot to enter a symbol"
+    elsif
     
+      if params[:id]
+       begin  
+        @stock = StockQuote::Stock.quote(params[:id])
+       rescue 
+        @error = "stock does not exist"
+       end
+      end
+    end
   end
   
   def about
-    
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'home/index'
   root "home#index"
   get 'home/about'
+  post "/" => 'home#index'
+  
   
   
 
